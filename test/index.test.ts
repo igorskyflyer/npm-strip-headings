58065ce1cb947315503b694894477e7ec45eaca2
igorskyflyer <igor.dvlpr@gmail.com>
2023-03-27 22:18:29 +0200

    Add new tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,20 @@
 import { assert as chai } from 'chai'
+import { stripHeadings, stripHeadingsCode } from './../src/index'
 
-describe('🧪 $1 tests 🧪', () => {
-  it('#1 should return ', () => {
-    //
+describe('🧪 strip-headings tests 🧪', () => {
+  it('#1 should return true', () => {
+    chai.isEmpty(stripHeadings(''))
+  })
+
+  it('#2 should return true', () => {
+    chai.isEmpty(stripHeadingsCode(''))
+  })
+
+  it('#3 should return empty', () => {
+    chai.isEmpty(stripHeadings('## Hello'))
+  })
+
+  it('#4 should return Hello', () => {
+    chai.equal(stripHeadingsCode('## Hello'), 'Hello')
   })
 })
